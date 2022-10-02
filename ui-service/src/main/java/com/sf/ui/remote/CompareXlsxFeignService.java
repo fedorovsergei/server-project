87ddc05a134fa304bc1df3f2d7bd3bbe7558fc38
s.fedorov <Brick-Glad-Forward-Yellow>
2022-10-02 15:09:55 +0300

    refactor

--- ui-service/src/main/java/com/sf/ui/remote/CompareXlsxFeignService.java
+++ ui-service/src/main/java/com/sf/ui/remote/CompareXlsxFeignService.java
@@ -5,7 +5,6 @@ import com.sf.ui.remote.dto.CompareXlsxServiceResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "compare-xlsx-service", url = "#{applicationProperties.compareXlsxServiceUrl}", path = "/")
 public interface CompareXlsxFeignService {
