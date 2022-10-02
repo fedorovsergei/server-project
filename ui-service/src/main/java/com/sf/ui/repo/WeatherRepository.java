7eb099a508874028cd1c7d1767f16562adb78248
s.fedorov <Brick-Glad-Forward-Yellow>
2022-10-02 16:05:50 +0300

    refactor

--- ui-service/src/main/java/com/sf/ui/repo/WeatherRepository.java
+++ ui-service/src/main/java/com/sf/ui/repo/WeatherRepository.java
@@ -2,6 +2,8 @@ package com.sf.ui.repo;
 
 import com.sf.ui.entity.WeatherHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -9,5 +11,8 @@ import java.time.LocalDateTime;
 @Repository
 public interface WeatherRepository extends JpaRepository<WeatherHistory, Long> {
 
-    WeatherHistory findByTime(LocalDateTime now);
+    @Query(value = """
+            select * from weather_history where time < :time order by time desc limit 1
+            """, nativeQuery = true)
+    WeatherHistory findByTime(@Param(value = "time") LocalDateTime time);
 }
--- ui-service/src/main/java/com/sf/ui/service/WeatherService.java
+++ ui-service/src/main/java/com/sf/ui/service/WeatherService.java
@@ -22,19 +22,13 @@ public class WeatherService {
 
     @Transactional
     public WeatherHistory getWeather() {
-        LocalDateTime now = roundTime(LocalDateTime.now(ZoneId.of("Europe/Moscow")));
-        log.info("start search weather to date {}", now);
-        WeatherHistory weatherHistory = weatherRepository.findByTime(now);
+        log.info("start search weather");
+        WeatherHistory weatherHistory = weatherRepository.findByTime(LocalDateTime.now(ZoneId.of("Europe/Moscow")));
         if (weatherHistory == null) {
             log.error("weather not found in db, call remote page");
             throw new NotFoundException("weather not found");
         }
         log.info("return find weather for {}", weatherHistory.getTime());
         return Objects.requireNonNull(weatherHistory);
-    }
-
-    private LocalDateTime roundTime(LocalDateTime now) {
-        return now.truncatedTo(ChronoUnit.HOURS)
-                .plusMinutes(10 * (now.getMinute() / 10));
     }
 }
